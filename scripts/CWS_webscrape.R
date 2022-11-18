e77c9223f4795e213de6dc5dd5d039bc6f79198f
Christine Parisek <christineparisek@gmail.com>
2022-11-18 09:52:04 -0800

    retitle files & work in webscrape script

--- scripts/CWS_webscrape.R
+++ scripts/CWS_webscrape.R
@@ -7,37 +7,94 @@ library(tidyverse)
 library(rvest)
 library(htm2txt)
 library(stringr)
+library(xml2)
 
-#read-in
+blog1<-read_csv("data/cws/2022-10-22 - All_Time_View_Stats_californiawaterblog.com_posts_day_10_22_2022_10_22_2022.csv")
+colnames(blog1)
 
-blog<-read_csv("data/cws/2022-10-22 - All_Time_View_Stats_californiawaterblog.com_posts_day_10_22_2022_10_22_2022.csv")
+#add dates to all blogs using URL string
+blog2<-blog1 %>% 
+  dplyr::mutate(date = substr(blog1$URL, 33, 42))
+
+#turn the date column into date "format" 
+blog2$date <- as.POSIXct( blog2$date, format="%Y/%m/%d" )
+print( blog2 )
+print( class(blog2$date) )
+
+#create column for author-name
+blog3<-blog2 
+
+
+
+#extract author in 2 different ways. end up taking body-text after BY. (prob same accuracy both ways.) 
+for(i in 1:nrow(blog3)){
+  # Read in the URL
+  page <- read_html(blog3$URL[i])
+  # See if there is text to be extracted from this xpath and the content attribute
+  author_text <- xml_find_all(page, '/html/head/meta[4]') %>% 
+    html_attr("content")
+  if(is.na(author_text)){
+    author_text <- xml_find_all(page, '/html/head/meta[4]') %>% 
+      html_text() 
+  } 
+  blog3$Author[i] <- author_text
+}
+
+sum(is.na(blog3$Author))
+
+for(i in 1:nrow(blog3)){
+  # Read in the URL
+  page <- read_html(blog3$URL[i])
+  # See if there is text to be extracted from this xpath and the content attribute
+  author_text <- xml_find_all(page, '/html/head/meta[4]') %>% 
+    html_attr("content") %>% 
+    #str_extract(., '(?<=[Bb]y:?\\s?)(\\w+\\s){3}') #includes colon
+    str_extract(., '(?<=[Bb]y?\\s?)(\\w+\\s){3}')
+  blog3$Author[i] <- author_text
+}
+
+sum(is.na(blog3$Author))
+
+#
+# this does similar/same to for-loop above. 
+#
+#for(i in 1:nrow(blog)){
+#  # Read in the URL
+#  page <- read_html(blog$URL[i])
+#  # See if there is text to be extracted from this xpath and the content attribute
+#  author_text <- as.data.frame(stringr::word(page, 2, sep="By"))
+#}
+
+
+
+
+blog3<-select(blog, -c(AllTimeViews))
+
+write_csv(blog3,"data output/blog_subset.csv")
+
+
+
+
+
+
+
+# Trying Something 1 --------------------------------------------------------------------
 
 starwars <- rvest::read_html("https://californiawaterblog.com/2022/10/16/being-patient-and-persistent-with-nature/")
 starwars
-
-#starwars <- rvest::read_html(blog$URL[1])
-#starwars
-
 url <- 'https://californiawaterblog.com/2022/10/16/being-patient-and-persistent-with-nature/'
 text <- htm2txt::gettxt(url)
 text<-as.data.frame(text)
 text
 author<-as.data.frame(stringr::word(text, 2, sep="By"))
 
-# Try2 --------------------------------------------------------------------
-
-
-library(rvest)
-library(dplyr)
-
+# Trying Something 2 --------------------------------------------------------------------
 
 url <- 'https://californiawaterblog.com/2022/10/16/being-patient-and-persistent-with-nature/'
 blog %>%
   mutate(text_found = purrr::map_lgl(url, ~  .x %>% read_html %>% 
                                        html_text() %>%  grepl('By', .)),
          Good_URL = !text_found)
-
-
 
 blog %>% 
   mutate(text_found = purrr::map_lgl(url, ~  .x %>% 
@@ -46,12 +103,37 @@ blog %>%
                                        grepl('By', .)),
          Good_URL = !text_found) 
 
-
-
 # https://stackoverflow.com/questions/44218814/extract-data-from-text-files-using-for-loop
 # https://stackoverflow.com/questions/54672672/r-loop-over-each-url-scrape-parse-extract-nodes-and-put-into-a-data-frame
 
 
 
+
+# Outtakes ----------------------------------------------------------------
+
+# Advice from Liza
+# (this works to grab 1st 3 words (in some cases) BUT author list is longer than 3 words (in most cases))
+# so I did not use this
+#
+#   for(i in 1:nrow(blog)){
+#     # Read in the URL
+#     page <- read_html(blog$URL[i])
+#     # See if there is text to be extracted from this xpath and the content attribute
+#     author_text <- xml_find_all(page, '/html/head/meta[4]') %>% 
+#       html_attr("content") %>% 
+#       # If so, this says take the text from content and then three words( (\\w+\\s){3} )
+#       # that come after some version of By ( [Bb]y:?\\s? means one of By:, by:, By, and by )
+#       # and the (?<=...) part is a 'lookaround' to tell stringr to look for the text after the BY part
+#       str_extract(., "(?<=[Bb]y:?\\s?)(\\w+\\s){3}")
+#     # But, if that xpath worked but it is in the text and not in that content attribute, try to just
+#     # extract the text using this alternative 
+#     if(is.na(author_text)){
+#       author_text <- xml_find_all(page, '/html/head/meta[4]') %>% 
+#         html_text() %>% 
+#         # Same regex expression to grab 3 words
+#         str_extract(., '(?<=[Bb]y:?\\s?)(\\w+\\s){3}')
+#     } 
+#     blog$Author[i] <- author_text
+#   }
 
 
